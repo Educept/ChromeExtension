0a5fdf72b6c7876cec41b18d1e009ac11f4a5133
Matin Hormati <mthormati@gmail.com>
2017-09-16 01:25:33 -0400

    Add button to turn extension on

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.querySelector('#extension-switch').addEventListener('mouseover', (e) => {
+  console.log('hi');
+  // const powerImage = document.querySelector('#extension-switch');
+  // powerImage.src = 'power_button_green.png';
+} );

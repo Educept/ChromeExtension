cef1c16223f27aeb9d754cf99b52e008a2502835
Matin Hormati <mthormati@gmail.com>
2017-09-16 10:02:04 -0400

    Build form

--- javascript/background.js
+++ javascript/background.js
@@ -1,3 +1,11 @@
-function saveOnOffState() {
-  
+function saveOnOffState(status) {
+  const items = {};
+  items['status'] =  status;
+  chrome.storage.sync.set(items);
+}
+
+function getOnOffState(callback) {
+  chrome.storage.sync.get('status', (items) => {
+    callback(chrome.runtime.lastError ? null : items['status']);
+  });
 }
--- javascript/popup.js
+++ javascript/popup.js
@@ -1,21 +1,57 @@
 class App {
   constructor(selectors) {
     const onSwitch = document.querySelector(selectors.extensionSwitch);
+    const form = document.querySelector(selectors.form);
 
     this.initializeRipple();
 
-    let isOn = false;
-
-    if (!isOn) {
-      const button = document.querySelector('#sign-in');
-      button.disabled = true;
-      button.dataset.opacity = 0;
-      button.style.background = '#424242';
-    }
+    let isOn;
+    this.getCurrentState();
 
     onSwitch.addEventListener('mouseover', this.changeImage.bind(this));
     onSwitch.addEventListener('mouseout', this.changeImage.bind(this));
-    onSwitch.addEventListener('click', this.turnOnExtension.bind(this));
+    onSwitch.addEventListener('click', this.switchState.bind(this));
+    form.addEventListener('submit', this.signIn.bind(this));
+  }
+
+  signIn(e) {
+    e.preventDefault();
+
+    const femail = document.querySelector('#email');
+    const fpassword = document.querySelector('#password');
+
+    $.ajax({
+      type: "POST",
+      url: 'http://10.33.2.152:3000/api/signin/',
+      data: {
+        'email': femail.value,
+        'password': fpassword.value
+      },
+      success: function(response) {
+        console.log(response);
+      }
+      // dataType: 'text'
+    })
+
+    femail.value = "";
+    fpassword.value = "";
+  }
+
+  getCurrentState() {
+    return getOnOffState((savedStatus) => {
+      this.isOn = savedStatus;
+      if (savedStatus) {
+        this.turnOnOffExtension();
+        return true;
+      } else {
+        const button = document.querySelector('#sign-in');
+        button.disabled = true;
+        button.dataset.opacity = 0;
+        button.style.background = '#424242';
+        this.turnOnOffExtension();
+        return false;
+      }
+    });
   }
 
   initializeRipple() {
@@ -43,18 +79,45 @@ class App {
     }
   }
 
-  turnOnExtension() {
-    const powerImage = document.querySelector('#extension-switch > img');
-    powerImage.src = 'assets/power_button_green.png';
-    this.isOn = true;
+  switchState(e) {
+    this.isOn = !this.isOn;
+    this.turnOnOffExtension();
+  }
 
-    const button = document.querySelector('#sign-in');
-    button.disabled = false;
-    button.dataset.opacity = 0.4;
-    button.style.background = '#2196F3';
+  turnOnOffExtension() {
+    if (this.isOn) {
+      const powerImage = document.querySelector('#extension-switch > img');
+      powerImage.src = 'assets/power_button_green.png';
+      this.isOn = true;
+
+      const button = document.querySelector('#sign-in');
+      button.disabled = false;
+      button.dataset.opacity = 0.4;
+      button.style.background = '#2196F3';
+
+      const inputs = document.querySelectorAll('input').forEach((input) => {
+        input.disabled = false;
+      });
+    } else {
+      const powerImage = document.querySelector('#extension-switch > img');
+      powerImage.src = 'assets/power_button.png';
+      this.isOn = false;
+
+      const button = document.querySelector('#sign-in');
+      button.disabled = true;
+      button.dataset.opacity = 0;
+      button.style.background = '#424242';
+
+      const inputs = document.querySelectorAll('input').forEach((input) => {
+        input.disabled = true;
+      });
+    }
+
+    saveOnOffState(this.isOn);
   }
 }
 
 new App({
   extensionSwitch: '#extension-switch',
+  form: '#sign-in-form',
 })
